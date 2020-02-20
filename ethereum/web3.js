8ee02809e9a2ed3f68bea8ed7fd8160541a1aca3
Manish <manishkumar13899@gmail.com>
2020-02-20 21:24:25 +0530

    Last deploy-2

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,18 +1,32 @@
 import Web3 from 'web3';
 
 let web3;
-
-if(typeof window !='undefined' && typeof window.web3 != 'undefined')
-{
-    web3 = new Web3(window.web3.currentProvider);
+if(typeof window!=='undefined' && typeof window.web3!=='undefined'){
+    window.addEventListener('load', async () => {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            try {
+                // Request account access if needed
+                await window.ethereum.enable();
+                // Acccounts now exposed
+                
+            } catch (error) {
+                console.log("Permission denied!")
+                
+            }
+        }
+        
+    });
+web3=new Web3(window.web3.currentProvider);
 }
-else
-{
+else{
+    console.log('a');
     const provider = new Web3.providers.HttpProvider(
-        // 'https://rinkeby.infura.io/v3/fefd36760efb4b1c9e94f04fa3f8e3f0'
-        'https://testnet2.matic.network'
-    );
-    web3 = new Web3(provider);
+		'https://testnet2.matic.network'
+	);
+    // const provider=new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/fecaf04a298945df852fef4c456adca3');
+    web3=new Web3(provider)
 }
 
 export default web3;
